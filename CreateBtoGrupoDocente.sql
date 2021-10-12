7fe838183a1c58a912427a4d8766adda7932be12
abcei2 <santiago.salgado.duque@gmail.com>
2021-10-12 14:21:31 -0500

    Create table to control grupo-docente and fill it

--- CreateBtoGrupoDocente.sql
+++ CreateBtoGrupoDocente.sql
@@ -10,10 +10,24 @@ create table BTO_GRUPO_DOCENTE(
 ALTER TABLE BTO_GRUPO_DOCENTE DROP CONSTRAINT BTO_GRUPO_DOCENTE_UNIQUES;
 ALTER TABLE BTO_GRUPO_DOCENTE add CONSTRAINT BTO_GRUPO_DOCENTE_UNIQUES UNIQUE (CURSO, GRUPO, DOCENTE_ID);
 
-INSERT INTO BTO_GRUPO_DOCENTE
-(CURSO, GRUPO, DOCENTE_ID )
-select CURSO, GRUPO,DOCENTE_ID from(
-    SELECT CURSO, '01' as GRUPO, DOCENTE_ID
-    FROM BTO_CURSO_DOCENTE group by CURSO, DOCENTE_ID 
-    FETCH FIRST 20000 ROWS ONLY -- I don't know why this is needed but whithout this, query doesnwork on insert.
-);  
+declare
+GRUPO_VAL varchar2(20);  
+BEGIN
+    FOR i IN 1..20 LOOP
+        IF i>=10 THEN
+            :GRUPO_VAL := TO_CHAR(i);
+        ELSE
+            :GRUPO_VAL := '0'||TO_CHAR(i);
+        END IF;    
+        INSERT INTO BTO_GRUPO_DOCENTE
+        (CURSO, GRUPO, DOCENTE_ID )
+        SELECT CURSO, :GRUPO_VAL as GRUPO, DOCENTE_ID
+        FROM BTO_CURSO_DOCENTE group by CURSO, DOCENTE_ID 
+        FETCH FIRST 20000 ROWS ONLY; -- I don't know why this is needed but whithout this, query doesnwork on insert.
+      END LOOP;
+END;
+
+commit
+
+
+
