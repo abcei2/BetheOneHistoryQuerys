20be06c60f4cb1f247558e7cb9ec8e71513bf358
abcei2 <santiago.salgado.duque@gmail.com>
2022-05-16 09:03:23 -0500

    .....

--- PARAPRODUCCION/BTO_META_NACIONAL.sql
+++ PARAPRODUCCION/BTO_META_NACIONAL.sql
@@ -1,9 +1,9 @@
 
-Insert into BTO_META_NACIONAL (CURSO,META) values ('4','PREA1');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('5','PREA1');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('6','A1');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('7','A2');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('8','A2');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('9','B1');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('10','B1');
-Insert into BTO_META_NACIONAL (CURSO,META) values ('11','B1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('9','4','PREA1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('10','5','PREA1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('11','6','A1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('12','7','A2');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('13','8','A2');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('14','9','B1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('15','10','B1');
+Insert into BTO_META_NACIONAL (CLAVE,CURSO,META) values ('16','11','B1');
